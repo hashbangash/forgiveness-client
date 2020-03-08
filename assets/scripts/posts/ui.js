a7510570c7350278882874062e3cb4699facbf92
ashtreehugs <agrev8@gmail.com>
2020-03-08 00:27:04 -0500

    Create form showing with handlebars!
    Ayyyy success!
    Still need to preventDefault and submit!
    But it's a great start!

--- assets/scripts/posts/ui.js
+++ assets/scripts/posts/ui.js
@@ -5,19 +5,14 @@ const store = require('./../store')
 const indexPostsTemplate = require('../templates/post-listing.handlebars')
 
 const onIndexPostsSuccess = function (response) {
-  if (store.user === undefined) {
-    console.log(response)
-  }
   if (store.routeFromCreatePost === true) {
-    $('#create-post-form').trigger('reset')
-    $('#create-post-form').hide()
+    $('#post-form').hide()
     $('#create-post-button').show()
     $('#message').text(`post successfully created!`)
   } else {
     $('#message').text(`generated existing posts!`)
   }
   const indexPostsHtml = indexPostsTemplate({ posts: response.posts })
-  console.log(indexPostsHtml)
   $('#post-board').html(indexPostsHtml)
 }
 
@@ -26,12 +21,7 @@ const failure = function (error) {
   $('#message').text(`sorry, error on our end. please try again.`)
 }
 
-const clearPosts = () => {
-  $('#post-board').empty()
-}
-
 module.exports = {
   onIndexPostsSuccess,
-  failure,
-  clearPosts
+  failure
 }
